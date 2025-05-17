6724863ed270348e67aab8823559ff76ae16f228
asadcodex <aslanrazasr@gmail.com>
2025-05-17 16:36:56 +0530

    Initial commit

---
+++ src/Todo.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+function Todo() {
+  const [tasks, setTasks] = useState([
+    "Eat Breakfast",
+    "Walk The Dog",
+    "Drink Water",
+  ]);
+  const [newTask, setNewTask] = useState("");
+
+  function addTask() {}
+
+  function deleteTask() {}
+
+  function moveTaskUp(index) {}
+
+  function moveTaskDown(index) {}
+
+  return (
+    <div className="to-do-list">
+      <h1>Todo List</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="Enter a task..."
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+        />
+        <button className="add-button" onClick={addTask}>
+          Add
+        </button>
+      </div>
+      <ol>
+        {tasks.map((task, index) => (
+          <li key={index}>
+            <span className="text">{task}</span>
+            <button className="delete-button" onClick={() => deleteTask(index)}>
+              Delete
+            </button>
+            <button className="move-button" onClick={() => moveTaskUp(index)}>
+              👆
+            </button>
+            <button className="move-button" onClick={() => moveTaskDown(index)}>
+              👇
+            </button>
+          </li>
+        ))}
+      </ol>
+    </div>
+  );
+}
+
+export default Todo;
